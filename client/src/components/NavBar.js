06af409739a3c7fdd18ee299e098c0be7e1d00d9
Phong <PhongTran3@my.unt.edu>
2023-12-02 22:24:25 -0600

    Added Navbar icons to project

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,23 @@
 import React from "react";
+import IconSpotify from "../icons/spotify";
+import IconUser from "../icons/user";
+import IconPlaylist from "../icons/playlist";
+import IconMusic from "../icons/music";
+import IconGithub from "../icons/github";
 
+import IconHistory from "../icons/history";
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <>
+      <div>
+        <h1>NavBar</h1>
+        <IconSpotify />
+        <IconUser />
+        <IconPlaylist />
+        <IconMusic />
+        <IconGithub />
+        <IconHistory />
+      </div>
+    </>
+  );
 }
