c2f322dfaa9e587eb32b21d851fe1907a3e36100
Phong <PhongTran3@my.unt.edu>
2023-12-09 23:04:38 -0600

    Change track container to div and added links to track meta

--- client/src/components/TrackItem.js
+++ client/src/components/TrackItem.js
@@ -9,64 +9,6 @@ import mixins from "../styles/mixins";
 import media from "../styles/media";
 const { colors, fontSizes, spacing } = theme;
 
-const TrackLeft = styled.span`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  padding-right: 1px;
-`;
-const TrackRight = styled.span``;
-const TrackArtwork = styled.div`
-  display: inline-block;
-  position: relative;
-  margin-right: ${spacing.base};
-  width: 150px;
-  min-width: 150px;
-  height: 150px;
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const TrackName = styled.span`
-  margin-bottom: 5px;
-  border-bottom: 1px solid transparent;
-  font-weight: 700;
-  &:hover,
-  &:focus {
-    border-bottom: 1px solid ${colors.white};
-  }
-`;
-const TrackMeta = styled.div`
-  display: grid;
-  grid-template-columns: 1fr max-content;
-  height: 100%;
-  grid-gap: 10px;
-`;
-
-const TrackAlbum = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  padding-right: 1px;
-  color: ${colors.lightGrey};
-  font-size: ${fontSizes.sm};
-  margin-top: ${spacing.sm};
-  span {
-    border-bottom: 1px solid transparent;
-    transition: border-bottom 0.3s;
-    &:hover,
-    &:focus {
-      border-bottom: 1px solid ${colors.white};
-    }
-  }
-`;
-
-const TrackDuration = styled.span`
-  color: ${colors.lightGrey};
-  font-size: ${fontSizes.sm};
-`;
 const Mask = styled.div`
   ${mixins.flexCenter};
   position: absolute;
@@ -85,11 +27,24 @@ const Mask = styled.div`
   }
 `;
 
-const TrackContainer = styled(Link)`
-  display: grid;
-  grid-template-columns: auto 1fr;
-  align-items: center;
-  margin-bottom: ${spacing.md};
+const TrackLeft = styled.span`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-right: 1px;
+`;
+const TrackRight = styled.span``;
+const TrackArtwork = styled(Link)`
+  display: inline-block;
+  position: relative;
+  margin-right: ${spacing.base};
+  width: 150px;
+  min-width: 150px;
+  height: 150px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
   &:hover,
   &:focus {
     ${Mask} {
@@ -98,12 +53,70 @@ const TrackContainer = styled(Link)`
   }
 `;
 
+const TrackName = styled(Link)`
+  margin-bottom: 5px;
+  border-bottom: 1px solid transparent;
+  font-weight: 700;
+  &:hover,
+  &:focus {
+    border-bottom: 1px solid ${colors.white};
+  }
+`;
+
+const TrackMeta = styled.div`
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  height: 100%;
+  grid-gap: 10px;
+`;
+
+const AlbumLinks = styled(Link)`
+  border-bottom: 1px solid transparent;
+  transition: border-bottom 0.3s;
+  &:hover,
+  &:focus {
+    border-bottom: 1px solid ${colors.white};
+  }
+`;
+const TrackAlbum = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-right: 1px;
+  color: ${colors.lightGrey};
+  font-size: ${fontSizes.sm};
+  margin-top: ${spacing.sm};
+  // span {
+  //   border-bottom: 1px solid transparent;
+  //   transition: border-bottom 0.3s;
+  //   &:hover,
+  //   &:focus {
+  //     border-bottom: 1px solid ${colors.white};
+  //   }
+  // }
+`;
+
+const TrackDuration = styled.span`
+  color: ${colors.lightGrey};
+  font-size: ${fontSizes.sm};
+`;
+
+const TrackContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  align-items: center;
+  margin-bottom: ${spacing.md};
+`;
+
 export default function TrackItem({ track }) {
+  //console.log(track.album.external_urls.spotify);
+  //track.artists[i].external_urls.spotify
+  //track.album.external_urls.spotify
   return (
     <li>
-      <TrackContainer to={`${track.external_urls.spotify}`} target="_blank" rel="noopener noreferrer">
+      <TrackContainer>
         <div>
-          <TrackArtwork>
+          <TrackArtwork to={`${track.external_urls.spotify}`} target="_blank" rel="noopener noreferrer">
             {track.album.images.length && <img src={track.album.images[1].url} alt="Album Artwork" />}
             <Mask>
               <IconInfo />
@@ -112,18 +125,24 @@ export default function TrackItem({ track }) {
         </div>
         <TrackMeta>
           <TrackLeft>
-            {track.name && <TrackName>{track.name}</TrackName>}
+            {track.name && (
+              <TrackName to={`${track.external_urls.spotify}`} target="_blank" rel="noopener noreferrer">
+                {track.name}
+              </TrackName>
+            )}
             {track.artists && track.album && (
               <TrackAlbum>
                 {track.artists &&
                   track.artists.map(({ name }, i) => (
-                    <span key={i}>
+                    <AlbumLinks to={`${track.artists[i].external_urls.spotify}`} key={i} target="_blank" rel="noopener noreferrer">
                       {name}
                       {track.artists.length > 0 && i === track.artists.length - 1 ? "" : ","}&nbsp;
-                    </span>
+                    </AlbumLinks>
                   ))}
                 &nbsp;&middot;&nbsp;&nbsp;
-                <span>{track.album.name}</span>
+                <AlbumLinks to={`${track.album.external_urls.spotify}`} target="_blank" rel="noopener noreferrer">
+                  {track.album.name}
+                </AlbumLinks>
               </TrackAlbum>
             )}
           </TrackLeft>
