ff458b601d1493eadf28321aab225346e41f73d9
Phong <PhongTran3@my.unt.edu>
2023-12-10 22:40:39 -0600

    Added inital imports

--- client/src/components/TopTracks.js
+++ client/src/components/TopTracks.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTopTracksLong, getTopTracksMedium, getTopTracksShort } from "../spotify";
+import StyledLoader from "../styles/Loader";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import mixins from "../styles/mixins";
+import Main from "../styles/Main";
+import media from "../styles/media";
 
+const { colors, fontSizes } = theme;
 export default function TopTracks() {
   return <div>TopTracks</div>;
 }
